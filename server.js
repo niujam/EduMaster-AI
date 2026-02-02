4739c7888174641270c86907e19ee7492ee2ef91
niujam <niujam@github.com>
2026-02-02 02:16:10 +0100

    Urdhri per te hapur index10 si faqe kryesore

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use(cors({
   origin: ['http://localhost:3000', 'http://localhost:8080', 'http://127.0.0.1:8080', 'http://127.0.0.1:3000', 'http://127.0.0.1:5500', 'http://localhost:5500', 'file://','´https://edumaster-ai.onrender.com'],
   credentials: true
 }));
-app.use(express.static(path.join(__dirname)));
+app.get('/', (req, res) => {res.sendFile(path.join(__dirname,'index10.html'));});
 // Stripe Webhook requires raw body, so we register it before JSON parsing
 const stripeSecretKey = process.env.STRIPE_SECRET_KEY;
 const stripeWebhookSecret = process.env.STRIPE_WEBHOOK_SECRET;
